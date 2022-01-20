3e7ad93e6aa24435969b9da29d56e5511a545427
Brad Gersh <bradley.gersh@gmail.com>
2022-01-20 21:07:14 +0100

    Several Lilypond commands added to grammar. Need to test.

--- Lilypond.g4
+++ Lilypond.g4
@@ -1,41 +1,99 @@
 grammar Lilypond;
 
-score_file  : score_block;
-score_block : SCORE_KW LBRACE staff* RBRACE;
-staff:
+// Multi-line blocks
+score_file: (
+        header_block
+        | book_block
+        | score_block
+        | version_cmd
+    )+;
+book_block   : BOOK_KW LBRACE score_block RBRACE;
+header_block : HEADER_KW LBRACE assignment* RBRACE;
+score_block  : SCORE_KW LBRACE staff_block* RBRACE;
+staff_block:
     STAFF_CTX with_block? LBRACE prefix_block? voice_block RBRACE;
-prefix_block : time_cmd;
-with_block   : WITH_KW LBRACE assignment* RBRACE;
-voice_block  : VOICE_CTX LBRACE note_block* RBRACE;
-note_block   : ( time_cmd | NOTE)+;
-time_cmd     : TIME_KW TIME_SIG;
-assignment   : ID '=' STRING;
+staffgroup_block : STAFFGROUP_CTX '<<' staff_block* '>>';
+prefix_block     : time_cmd;
+with_block       : WITH_KW LBRACE assignment* RBRACE;
+voice_block:
+    VOICE_CTX LBRACE (note_block | polyphony_block)* RBRACE;
+note_block     : ( relative_block | note_cmd | NOTE)+;
+relative_block : RELATIVE_KWD NOTE LBRACE note_block RBRACE;
+polyphony_block:
+    '<<' LBRACE note_block RBRACE '\\\\' LBRACE note_block RBRACE '>>';
+
+// One-line commands
+note_cmd:
+    bar_cmd
+    | clef_cmd
+    | fermata_cmd
+    | key_cmd
+    | mark_cmd
+    | tempo_cmd
+    | time_cmd;
+bar_cmd     : BAR_KW BARLINE;
+clef_cmd    : CLEF_KW ID;
+fermata_cmd : FERMATA_KW;
+key_cmd     : KEY_KW NOTE MODE;
+mark_cmd    : MARK_KW (SCM_TOKEN | DEFAULT_KW)?;
+tempo_cmd   : TEMPO_KW (STRING? TEMPO_EQN | STRING);
+time_cmd    : TIME_KW TIME_SIG;
+version_cmd : VERSION_KW VERSION_STR;
+assignment  : ID '=' STRING;
 
 // Contexts
-STAFF_CTX : '\\new Staff';
-VOICE_CTX : '\\new Voice';
+STAFF_CTX      : '\\new Staff';
+STAFFGROUP_CTX : '\\new StaffGroup';
+VOICE_CTX      : '\\new Voice';
 
-// Commands
-LAYOUT_KW : '\\layout';
-SCORE_KW  : '\\score';
-TIME_KW   : '\\time';
-WITH_KW   : '\\with';
-KW        : SLASH LETTER+;
+// Layout Commands
+BOOK_KW    : '\\book';
+HEADER_KW  : '\\header';
+LAYOUT_KW  : '\\layout';
+SCORE_KW   : '\\score';
+WITH_KW    : '\\with';
+VERSION_KW : '\\version';
 
+// Notation Commands
+BAR_KW      : '\\bar';
+CLEF_KW     : '\\clef';
+DEFAULT_KW  : '\\default';
+FERMATA_KW  : '\\fermata';
+ITALIC_KW   : '\\italic';
+KEY_KW      : '\\key';
+MARK_KW     : '\\mark';
+MARKUP_KW   : '\\markup';
+MODE_KW     : MODE_MAJ | MODE_MIN;
+MODE_MAJ    : '\\major';
+MODE_MIN    : '\\minor';
+RELATIVE_KW : '\\relative';
+TEMPO_KW    : '\\tempo';
+TIME_KW     : '\\time';
+
+KW : SLASH ID_CHAR+;
+
+// Note entry
+NOTE      : ID_CHAR OCTAVE? DURATION?;
 TIME_SIG  : INTEGER '/' INTEGER;
-STRING    : '"' LETTER+ '"';
-NOTE      : LETTER OCTAVE? DURATION?;
-OCTAVE    : '\''+ | ','+;
+TEMPO_EQN : INTEGER '=' INTEGER;
 DURATION  : INTEGER '.'?;
-ID        : (LETTER | '.' | '/')+;
-SCM_TOKEN : '#' .+? WS;
+OCTAVE    : '\''+ | ','+;
+BARLINE   : '"' BAR_CHAR+ '"';
 
-LBRACE          : '{';
-RBRACE          : '}';
-SLASH           : '\\';
-INTEGER         : DIGIT+;
-fragment LETTER : [a-zA-Z];
-fragment DIGIT  : [0-9];
+ID                   : (ID_CHAR | '.' | '/')+;
+STRING               : '"' STRING_CHAR+ '"';
+VERSION_STR          : '"' (INTEGER | '.')+ '"';
+SCM_TOKEN            : '#' .+? WS;
+LBRACE               : '{';
+RBRACE               : '}';
+SLASH                : '\\';
+INTEGER              : DIGIT+;
+fragment STRING_CHAR : [a-zA-Z ];
+fragment ID_CHAR     : [a-zA-Z];
+fragment DIGIT       : [0-9];
+fragment BAR_CHAR    : [|.;![\]];
 
-WS  : [ \t\r\n]+ -> skip;
-ALL : .+? -> skip;
+COMMENT_MULTILINE : '%{' .*? '%}';
+COMMENT_INLINE    : '%' .*? WS;
+WS                : [ \t\r\n]+ -> skip;
+ALL               : .+? -> skip;
---
+++ LilypondListener.py
@@ -0,0 +1,183 @@
+# Generated from Lilypond.g4 by ANTLR 4.9.3
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .LilypondParser import LilypondParser
+else:
+    from LilypondParser import LilypondParser
+
+# This class defines a complete listener for a parse tree produced by LilypondParser.
+class LilypondListener(ParseTreeListener):
+
+    # Enter a parse tree produced by LilypondParser#score_file.
+    def enterScore_file(self, ctx:LilypondParser.Score_fileContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#score_file.
+    def exitScore_file(self, ctx:LilypondParser.Score_fileContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#book_block.
+    def enterBook_block(self, ctx:LilypondParser.Book_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#book_block.
+    def exitBook_block(self, ctx:LilypondParser.Book_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#header_block.
+    def enterHeader_block(self, ctx:LilypondParser.Header_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#header_block.
+    def exitHeader_block(self, ctx:LilypondParser.Header_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#score_block.
+    def enterScore_block(self, ctx:LilypondParser.Score_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#score_block.
+    def exitScore_block(self, ctx:LilypondParser.Score_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#staff_block.
+    def enterStaff_block(self, ctx:LilypondParser.Staff_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#staff_block.
+    def exitStaff_block(self, ctx:LilypondParser.Staff_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#staffgroup_block.
+    def enterStaffgroup_block(self, ctx:LilypondParser.Staffgroup_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#staffgroup_block.
+    def exitStaffgroup_block(self, ctx:LilypondParser.Staffgroup_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#prefix_block.
+    def enterPrefix_block(self, ctx:LilypondParser.Prefix_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#prefix_block.
+    def exitPrefix_block(self, ctx:LilypondParser.Prefix_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#with_block.
+    def enterWith_block(self, ctx:LilypondParser.With_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#with_block.
+    def exitWith_block(self, ctx:LilypondParser.With_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#voice_block.
+    def enterVoice_block(self, ctx:LilypondParser.Voice_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#voice_block.
+    def exitVoice_block(self, ctx:LilypondParser.Voice_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#note_block.
+    def enterNote_block(self, ctx:LilypondParser.Note_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#note_block.
+    def exitNote_block(self, ctx:LilypondParser.Note_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#polyphony_block.
+    def enterPolyphony_block(self, ctx:LilypondParser.Polyphony_blockContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#polyphony_block.
+    def exitPolyphony_block(self, ctx:LilypondParser.Polyphony_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#bar_cmd.
+    def enterBar_cmd(self, ctx:LilypondParser.Bar_cmdContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#bar_cmd.
+    def exitBar_cmd(self, ctx:LilypondParser.Bar_cmdContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#clef_cmd.
+    def enterClef_cmd(self, ctx:LilypondParser.Clef_cmdContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#clef_cmd.
+    def exitClef_cmd(self, ctx:LilypondParser.Clef_cmdContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#fermata_cmd.
+    def enterFermata_cmd(self, ctx:LilypondParser.Fermata_cmdContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#fermata_cmd.
+    def exitFermata_cmd(self, ctx:LilypondParser.Fermata_cmdContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#mark_cmd.
+    def enterMark_cmd(self, ctx:LilypondParser.Mark_cmdContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#mark_cmd.
+    def exitMark_cmd(self, ctx:LilypondParser.Mark_cmdContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#tempo_cmd.
+    def enterTempo_cmd(self, ctx:LilypondParser.Tempo_cmdContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#tempo_cmd.
+    def exitTempo_cmd(self, ctx:LilypondParser.Tempo_cmdContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#time_cmd.
+    def enterTime_cmd(self, ctx:LilypondParser.Time_cmdContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#time_cmd.
+    def exitTime_cmd(self, ctx:LilypondParser.Time_cmdContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#version_cmd.
+    def enterVersion_cmd(self, ctx:LilypondParser.Version_cmdContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#version_cmd.
+    def exitVersion_cmd(self, ctx:LilypondParser.Version_cmdContext):
+        pass
+
+
+    # Enter a parse tree produced by LilypondParser#assignment.
+    def enterAssignment(self, ctx:LilypondParser.AssignmentContext):
+        pass
+
+    # Exit a parse tree produced by LilypondParser#assignment.
+    def exitAssignment(self, ctx:LilypondParser.AssignmentContext):
+        pass
+
+
+
+del LilypondParser
