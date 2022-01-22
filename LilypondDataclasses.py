e4c85a3f51cd2232f9fc7fc3d8ca1db564656f2b
Brad Gersh <bradley.gersh@gmail.com>
2022-01-22 10:17:46 +0100

    dataclasses added

--- LilypondDataclasses.py
+++ LilypondDataclasses.py
@@ -1,4 +1,3 @@
-from optparse import Option
 from typing import List, Optional
 from dataclasses import dataclass, field
 from enum import Enum, auto
@@ -17,12 +16,28 @@ class Node:
 class Group(Node):
     pass
 
-class MetadataField(Enum):
-    TITLE = Optional[auto()]
-    SUBTITLE = Optional[auto()]
-    COMPOSER = Optional[auto()]
+@dataclass
+class CommandName(Enum):
+    # There are many Lilypond commands I am omitting here.
+    # A fuller implementation should account for them all.
+    CONSISTS = auto()
+    OVERRIDE = auto()
+    REMOVE = auto()
+    SET = auto()
 
-class Metadata(Group):
+@dataclass
+class Command(Node):
+    command: CommandName
+    argument: str
+
+@dataclass
+class MetadataField(Enum):
+    TITLE = auto()
+    SUBTITLE = auto()
+    COMPOSER = auto()
+
+@dataclass
+class Metadata(Node):
     field: MetadataField
     value: str
 
@@ -39,17 +54,156 @@ class Version(Node):
     lilypond_ver: str
 
 @dataclass
+class WithCmd(Group):
+    commands: List(Command)
+
+
+@dataclass
+class NoteLength(Enum):
+    WHOLE = auto()
+    WHOLED = auto()
+    WHOLEDD = auto()
+    WHOLEDDD = auto()
+    HALF = auto()
+    HALFD = auto()
+    HALFDD = auto()
+    HALFDDD = auto()
+    QUARTER = auto()
+    QUARTERD = auto()
+    QUARTERDD = auto()
+    QUARTERDDD = auto()
+    EIGHTH = auto()
+    EIGHTHD = auto()
+    EIGHTHDD = auto()
+    EIGHTHDDD = auto()
+    SIXTEENTH = auto()
+    SIXTEENTHD = auto()
+    SIXTEENTHDD = auto()
+    SIXTEENTHDDD = auto()
+    THIRTYSECOND = auto()
+    THIRTYSECONDD = auto()
+    THIRTYSECONDDD = auto()
+    THIRTYSECONDDDD = auto()
+    SIXTYFOURTH = auto()
+    SIXTYFOURTHD = auto()
+    SIXTYFOURTHDD = auto()
+    SIXTYFOURTHDDD = auto()
+    ONETWENTYEIGHTH = auto()
+    ONETWENTYEIGHTHD = auto()
+    ONETWENTYEIGHTHDD = auto()
+    ONETWENTYEIGHTHDDD = auto()
+    TWOFIFTYSIXTH = auto()
+    TWOFIFTYSIXTHD = auto()
+    TWOFIFTYSIXTHDD = auto()
+    TWOFIFTYSIXTHDDD = auto()
+
+@dataclass
+class PitchClass(Enum):
+    AESES = auto()
+    AES = auto()
+    A = auto()
+    AIS = auto()
+    AISIS = auto()
+    BESES = auto()
+    BES = auto()
+    B = auto()
+    BIS = auto()
+    BISIS = auto()
+    CESES = auto()
+    CES = auto()
+    C = auto()
+    CIS = auto()
+    CISIS = auto()
+    DESES = auto()
+    DES = auto()
+    D = auto()
+    DIS = auto()
+    DISIS = auto()
+    ESES = auto()
+    ES = auto()
+    E = auto()
+    EIS = auto()
+    EISIS = auto()
+    FESES = auto()
+    FES = auto()
+    F = auto()
+    FIS = auto()
+    FISIS = auto()
+    GESES = auto()
+    GES = auto()
+    G = auto()
+    GIS = auto()
+    GISIS = auto()
+
+@dataclass
+class Mode(Enum):
+    MAJOR = auto()
+    MINOR = auto()
+    IONIAN = auto()
+    DORIAN = auto()
+    PHRYGIAN = auto()
+    LYDIAN = auto()
+    MIXOLYDIAN = auto()
+    AEOLIAN = auto()
+    LOCRIAN = auto()
+
+@dataclass
+class Time(Node):
+    numerator: int
+    denominator: NoteLength
+
+@dataclass
+class Key(Node):
+    note: PitchClass
+    mode: Mode
+
+@dataclass
+class Pitch(Node):
+    pitch_class: PitchClass
+    octave: Number
+
+@dataclass
+class Extra(Enum):
+    CLEF_BASS = auto()
+    CLEF_TREBLE = auto()
+    FERMATA = auto()
+    MARK = auto()
+    BAR_SINGLE = auto()
+    BAR_DOUBLE = auto()
+    BAR_FINAL = auto()
+
+@dataclass
+class Chord(Node):
+    pitch: Pitch
+    length: NoteLength
+    extras: Optional[List(Extra)]
+
+@dataclass
+class Tempo(Node):
+    desc: str
+    unit = NoteLength
+    per_min = int
+
+@dataclass
+class NoteGroup(Group):
+    time: Optional[Time]
+    key: Optional[Key]
+    tempo: Optional[Tempo]
+    notes = List(Chord)
+
+@dataclass
 class Staff(Group):
     with_cmd: Optional[WithCmd]
-    prefix: Optional[Prefix]
-    notes: Notes
+    notes: NoteGroup
 
 @dataclass
 class StaffGroup(Group):
     staves: List(Staff)
+
 @dataclass
 class Score(Group):
     staff_groups: List(StaffGroup)
+
 @dataclass
 class Score_File(Group):
     header: Optional[Header]
--- README.md
+++ README.md
@@ -11,7 +11,8 @@ At the command line, enter
 python ly2musicxml.py /path/to/FILE.ly
 ```
 
-where `FILE.ly` is your Lilypond source to convert to MusicXML. A sample file, `sample.ly`, is included in the repository as a demonstration.
+where `FILE.ly` is your Lilypond source to convert to MusicXML. A sample file,
+`sample.ly`, is included in the repository as a demonstration.
 
 ## Supported Commands
 
@@ -35,7 +36,10 @@ the following commands should parse correctly:
   \bar, \clef, \default, \key, \mark, \major, \minor, \relative, \tempo, \time
   ```
 
-  Polyphony blocks (delimited by `<< >>`) and chords (delimited by `< >`) are also possible.
+  Chords (delimited by `< >`) are also possible. At present, rhythms cannot
+  involve tuplets, and repeat signs are not supported. The parser can recognize
+  polyphony blocks (delimited by `<< >>`), but currently only the top line is
+  transpiled into MusicXML.
 
 ## Credits
 
@@ -43,6 +47,7 @@ This implementation was developed by Bradley Gersh and released under the MIT
 License. Development is ongoing, but if you have suggestions or bug reports,
 please feel free to let me know!
 
-The parser was generated using Terence Parr and Sam Harwell's [ANTLR4](https://www.antlr.org/).
-My model for the AST code is based on [Strumenta's](https://strumenta.com/) course,
+The parser was generated using Terence Parr and Sam Harwell's
+[ANTLR4](https://www.antlr.org/). My model for the AST code is based on
+[Strumenta's](https://strumenta.com/) course,
 ["Using ANTLR Like a Professional," Professional Edition](https://tomassetti.me/antlr-course-2-edition/).
